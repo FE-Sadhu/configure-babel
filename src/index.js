232d1ba08e4d9a1f3e454f31f5c4fe1581a378ec
sadhu <1714222385@qq.com>
2021-04-20 00:31:50 +0800

    此时 lib 是污染全局环境的 polyfill

---
+++ src/index.js
@@ -0,0 +1,28 @@
+[].flat(3)
+
+Promise.resolve().then(res => res)
+
+const wk = new WeakMap();
+
+() => {
+  console.log(a);
+}
+
+a?.b
+
+const isHas = [1,2,3].includes(2);
+
+async () => {
+  await Promise.resolve()
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  getX() {
+    return this.x;
+  }
+}
+let cp = new ColorPoint(25, 8);
---
+++ src/two.js
@@ -0,0 +1,16 @@
+[].flat(3);
+
+Promise.resolve().then((res) => res);
+
+const wk = new WeakMap();
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  getX() {
+    return this.x;
+  }
+}
+let cp = new ColorPoint(25, 8);
